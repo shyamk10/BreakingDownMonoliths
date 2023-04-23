877662407296dff2e7c126b039851077216fccd5
sanjita811 <79326586+sanjita811@users.noreply.github.com>
2023-04-23 12:10:28 +0530

    Subtract microservice

---
+++ minus/app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class minus(Resource):
+    def get(self, num1, num2):
+        result = num1 - num2
+        return {'result': result}
+
+
+api.add_resource(minus, '/<float:num1>/<float:num2>')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5050, host='0.0.0.0')
